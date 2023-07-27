3763ee4292d4a3b7dc006809d7a54031c54c18cd
EricLam404 <lameric9888@gmail.com>
2023-07-27 19:56:10 -0400

    Added skills for web development

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,32 +1,83 @@
-import React from 'react'
+import React from 'react';
+import { JSIcon, PythonIcon, CppIcon } from './Icons';
+import { HTMLIcon, CSSIcon, TailwindCSSIcon, ReactIcon, NextIcon } from './Icons';
+import { NodeIcon, ExpressIcon, PrismaIcon } from './Icons';
+import { MongoDBIcon, FirebaseIcon, SQLIcon, PostgreSQLIcon } from './Icons';
+import { JestIcon, DoctestIcon } from './Icons';
+//import { JSIcon, PythonIcon, CppIcon } from './Icons';
 
-//temp skills section
-const skills = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "C/C++",
-    "Python",
-    "React.js",
-    "Node.js",
-    "Express.js",
-    "Next.js",
-    "Tailwind CSS",
-];
+const languages = [
+    {name: "JavaScript", icon: <JSIcon className="w-[64px] h-[64px]" />},
+    {name: "Python", icon: <PythonIcon className="w-[64px] h-[64px]" />},
+    {name: "C/C++", icon: <CppIcon className="w-[64px] h-[64px]" />},
+]
+
+const frontend = [
+    {name: "HTML", icon: <HTMLIcon className="w-[64px] h-[64px]" />},
+    {name: "CSS", icon: <CSSIcon className="w-[64px] h-[64px]" />},
+    {name: "TailwindCSS", icon: <TailwindCSSIcon className="w-[64px] h-[64px]" />},
+    {name: "React.js", icon: <ReactIcon className="w-[64px] h-[64px]" />},
+    {name: "Next.js", icon: <NextIcon className="w-[64px] h-[64px]" />},
+]
+
+const backend = [
+    {name: "Node.js", icon: <NodeIcon className="w-[64px] h-[64px]" />},
+    {name: "Express.js", icon: <ExpressIcon className="w-[64px] h-[64px]" />},
+    {name: "Prisma", icon: <PrismaIcon className="w-[64px] h-[64px]" />},
+]
+
+const database = [
+    {name: "MongoDB", icon: <MongoDBIcon className="w-[64px] h-[64px]" />},
+    {name: "Firebase", icon: <FirebaseIcon className="w-[64px] h-[64px]" />},
+    {name: "SQL", icon: <SQLIcon className="w-[64px] h-[64px]" />},
+    {name: "PostgreSQL", icon: <PostgreSQLIcon className="w-[64px] h-[64px]" />},
+]
+
+const testing = [
+    {name: "Jest", icon: <JSIcon className="w-[64px] h-[64px]" />},
+    {name: "Doctest", icon: <PythonIcon className="w-[64px] h-[64px]" />},
+]
+
+const other = [
+    {name: "Git", icon: <JSIcon className="w-[64px] h-[64px]" />},
+    {name: "Makefile", icon: <JSIcon className="w-[64px] h-[64px]" />},
+]
+
+
+const Card = ({type, skills}) => {
+    return (
+        <article className='w-full col-span-4 flex flex-col items-center rounded-2xl border border-solid border-dark bg-light shadow-2xl p-6 relative dark:bg-dark dark:border-light xs:p-4 lg:col-span-6 md:col-span-12'>
+                
+            <div className='w-full flex flex-col items-center dark:bg-dark'>
+                <div className='text-dark font-bold text-3xl mb-5 dark:text-light lg:text-2xl md:text-xl'>{type}</div>
+                <ul className='flex flex-col items-center gap-5'>
+                {skills.map((skill) => 
+                    <li key={skill.name} className='flex flex-col justify-center items-center'>
+                        {skill.icon}
+                        <div>{skill.name}</div>
+                    </li>
+                )}
+            </ul>
+            </div>
+        </article>
+    )
+}
 
 const Skills = () => {
 return (
-    <div className="bg-gray-100 p-8">
-        <h2 className="text-3xl font-bold mb-4">Skills</h2>
-        <ul className="list-disc ml-8">
-            {skills.map((skill) => (
-            <li key={skill} className="my-2">
-                {skill}
-            </li>
-            ))}
-        </ul>
+    <div className="w-full bg-gray-100 p-8 pt-12 flex flex-col items-center dark:bg-dark dark:text-light">
+        <h2 className="pb-6 text-7xl font-bold my-6 lg:!text-7xl sm:!text-6xl xs:!text-4xl">Skills</h2>
+        <div className="w-full grid grid-cols-12 gap-16 gap-y-16">
+            <Card type="Languages" skills={languages} />
+            <Card type="Frontend" skills={frontend} />
+            <Card type="Backend" skills={backend} />
+            <Card type="Databases" skills={database} />
+            <Card type="Version Control" skills={testing} />
+            <Card type="Testing" skills={other} />
+        </div>
     </div>
     );
 };
 
 export default Skills
+

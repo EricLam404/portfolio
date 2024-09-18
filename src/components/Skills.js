a2dcc46eb2c803b9babbb198e2686621b9a2b3fc
EricLam404 <lameric9888@gmail.com>
2024-09-18 16:36:56 -0400

    Refactor Skills component and About page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,37 +1,47 @@
 import React from 'react';
-import { JSIcon, PythonIcon, CppIcon } from './Icons';
-import { HTMLIcon, CSSIcon, TailwindCSSIcon, ReactIcon, NextIcon } from './Icons';
-import { NodeIcon, ExpressIcon, PrismaIcon } from './Icons';
-import { MongoDBIcon, FirebaseIcon, SQLIcon, PostgreSQLIcon } from './Icons';
+import { JSIcon, PythonIcon, CppIcon, HTMLIcon, CSSIcon, SQLIcon, GoIcon, DartIcon} from './Icons';
+import { TailwindCSSIcon, ReactIcon, NextIcon, FlutterIcon } from './Icons';
+import { NodeIcon, ExpressIcon, PthreadsIcon, OpenMPIIcon, RestAPIIcon, GraphQLIcon } from './Icons';
+import { MongoDBIcon, FirebaseIcon, PostgreSQLIcon, PrismaIcon, SupabaseIcon, Neo4jIcon } from './Icons';
 import { JestIcon, DoctestIcon } from './Icons';
-import { GitIcon, GithubIcon, MakefileIcon, NPMIcon, WebpackIcon } from './Icons';
+import { GitIcon, GithubIcon, MakefileIcon, NPMIcon, WebpackIcon, DockerIcon, GCPIcon } from './Icons';
 
 const size = "!w-[64px] !h-[64px] sm:!w-[48px] sm:!h-[48px] xs:!w-[40px] xs:!h-[40px]";
 const languages = [
-    {name: "JavaScript", icon: <JSIcon className={`${size}`} />},
+    {name: "JavaScript/Typescript", icon: <JSIcon className={`${size}`} />},
     {name: "Python", icon: <PythonIcon className={`${size}`} />},
     {name: "C/C++", icon: <CppIcon className={`${size}`} />},
+    {name: "HTML", icon: <HTMLIcon className={`${size}`} />},
+    {name: "CSS", icon: <CSSIcon className={`${size}`} />},
+    {name: "SQL", icon: <SQLIcon className={`${size}`} />},
+    {name: "Go", icon: <GoIcon className={`${size}`} />},
+    {name: "Dart", icon: <DartIcon className={`${size}`} />},
 ]
 
 const frontend = [
-    {name: "HTML", icon: <HTMLIcon className={`${size}`} />},
-    {name: "CSS", icon: <CSSIcon className={`${size}`} />},
     {name: "TailwindCSS", icon: <TailwindCSSIcon className={`${size}`} />},
     {name: "React.js", icon: <ReactIcon className={`${size}`} />},
     {name: "Next.js", icon: <NextIcon className={`${size}`} />},
+    {name: "Flutter", icon: <FlutterIcon className={`${size}`} />},
 ]
 
 const backend = [
     {name: "Node.js", icon: <NodeIcon className={`${size}`} />},
     {name: "Express.js", icon: <ExpressIcon className={`${size}`} />},
-    {name: "Prisma", icon: <PrismaIcon className={`${size} dark:bg-dark rounded-2xl p-2`} />},
+    {name: "Go", icon: <GoIcon className={`${size}`} />},
+    {name: "Pthreads", icon: <PthreadsIcon className={`${size}`} />},
+    {name: "OpenMPI", icon: <OpenMPIIcon className={`${size}`} />},
+    {name: "REST APIs", icon: <RestAPIIcon className={`${size}`} />},
+    {name: "Graphql", icon: <GraphQLIcon className={`${size}`} />},
 ]
 
 const database = [
     {name: "MongoDB", icon: <MongoDBIcon className={`${size}`} />},
     {name: "Firebase", icon: <FirebaseIcon className={`${size}`} />},
-    {name: "SQL", icon: <SQLIcon className={`${size}`} />},
     {name: "PostgreSQL", icon: <PostgreSQLIcon className={`${size}`} />},
+    {name: "Prisma", icon: <PrismaIcon className={`${size} dark:bg-dark rounded-2xl p-2`} />},
+    {name: "Supabase", icon: <SupabaseIcon className={`${size}`} />},
+    {name: "Neo4j", icon: <Neo4jIcon className={`${size}`} />},
 ]
 
 const testing = [
@@ -43,8 +53,9 @@ const miscellaneous = [
     {name: "Git", icon: <GitIcon className={`${size}`} />},
     {name: "Github", icon: <GithubIcon className={`${size} border border-solid rounded-full dark:bg-dark`} />},
     {name: "Makefile", icon: <MakefileIcon className={`${size}`} />},
-    {name: "NPM", icon: <NPMIcon className={`${size} pt-5`} />},
     {name: "Webpack", icon: <WebpackIcon className={`${size}`} />},
+    {name: "Docker", icon: <DockerIcon className={`${size}`} />},
+    {name: "Google Cloud Platform", icon: <GCPIcon className={`${size}`} />},
 ]
 
 
@@ -68,7 +79,7 @@ const Card = ({type, skills}) => {
 
 const Skills = () => {
 return (
-    <div className="w-full bg-gray-100 p-8 pt-12 flex flex-col items-center dark:bg-dark dark:text-light">
+    <div className="w-full bg-gray-100 p-8 mt-12 flex flex-col items-center dark:bg-dark dark:text-light">
         <h2 className="pb-6 text-7xl font-bold my-6 lg:!text-6xl sm:!text-5xl xs:!text-3xl">Skills</h2>
         <div className="w-full grid grid-cols-12 gap-16 gap-y-16 lg:gap-10 sm:gap-8 xs:gap-6">
             <Card type="Languages" skills={languages} />
